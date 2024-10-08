74587e78b2d505ca869de0c5db1dab6eb80e086c
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-10-08 15:54:24 +0700

    up

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/kizaru-web/" : "/",
+  publicPath: "/",
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
